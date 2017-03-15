5bc0ddb5e96e36d3c1773eb6d6c544514a2b506b
Daniel Semblano <dsemblano@gmail.com>
2017-03-15 17:17:39 -0300

    correcao section mais extendida e botão demais materias, linkando para página de todas categorias

--- site/web/app/themes/ii/templates/partials/home-mais.php
+++ site/web/app/themes/ii/templates/partials/home-mais.php
@@ -24,23 +24,16 @@
     <section id="historia-do-brasil">
       <h2>História do Brasil</h2>
     <?php
-      $args = new WP_Query( array( 'category_name' => 'historia-do-brasil', 'orderby' => 'rand', 'posts_per_page'=>1 ) );
+      $args = new WP_Query( array( 'category_name' => 'historia-do-brasil', 'orderby' => 'rand', 'posts_per_page'=>4 ) );
+      $row = 1;
       if ( $args->have_posts() ): while ( $args->have_posts() ) : $args->the_post();
     ?>
       <article>
-        <a href="<?php the_permalink(); ?>">
-          <?php echo (App\featured_image_url('thumb-mais')); ?>
+        <a class="<?=$row; ?>" href="<?php the_permalink(); ?>">
+          <?php if ($row == 1): ?>
+            <?php echo (App\featured_image_url('thumb-mais')); ?>
+          <?php endif; ++$row; ?>
           <h3><?php the_title(); ?></h3>
-          <?php
-              if (!has_excerpt()) {
-              $content = apply_filters('get_the_content', get_the_content());
-              $content = strip_tags($content);
-              $content = mb_strimwidth($content, 0, 500, ' ...');
-              echo '<p>' . $content . '</p>';
-              } else {
-              echo '<p>' . substr(get_the_excerpt(), 0, 450) . '...' . '</p>';
-              }
-          ?>
         </a>
       </article>
     <?php endwhile; ?>
@@ -60,16 +53,6 @@
         <a href="<?php the_permalink(); ?>">
           <?php echo (App\featured_image_url('thumb-mais')); ?>
           <h3><?php the_title(); ?></h3>
-          <?php
-              if (!has_excerpt()) {
-              $content = apply_filters('get_the_content', get_the_content());
-              $content = strip_tags($content);
-              $content = mb_strimwidth($content, 0, 500, ' ...');
-              echo '<p>' . $content . '</p>';
-              } else {
-              echo '<p>' . substr(get_the_excerpt(), 0, 450) . '...' . '</p>';
-              }
-          ?>
         </a>
       </article>
     <?php endwhile; ?>
@@ -92,16 +75,6 @@
         <a href="<?php the_permalink(); ?>">
           <?php echo (App\featured_image_url('thumb-mais')); ?>
           <h3><?php the_title(); ?></h3>
-          <?php
-              if (!has_excerpt()) {
-              $content = apply_filters('get_the_content', get_the_content());
-              $content = strip_tags($content);
-              $content = mb_strimwidth($content, 0, 500, ' ...');
-              echo '<p>' . $content . '</p>';
-              } else {
-              echo '<p>' . substr(get_the_excerpt(), 0, 450) . '...' . '</p>';
-              }
-          ?>
         </a>
       </article>
     <?php endwhile; ?>
@@ -114,23 +87,16 @@
     <section id="crime-organizado">
       <h2>Crime Organizado</h2>
     <?php
-      $args = new WP_Query( array( 'category_name' => 'crime-organizado', 'orderby' => 'rand', 'posts_per_page'=>1 ) );
+      $args = new WP_Query( array( 'category_name' => 'crime-organizado', 'orderby' => 'rand', 'posts_per_page'=>4 ) );
+      $row = 1;
       if ( $args->have_posts() ): while ( $args->have_posts() ) : $args->the_post();
     ?>
       <article>
-        <a href="<?php the_permalink(); ?>">
-          <?php echo (App\featured_image_url('thumb-mais')); ?>
+        <a class="<?=$row; ?>" href="<?php the_permalink(); ?>">
+          <?php if ($row == 1): ?>
+            <?php echo (App\featured_image_url('thumb-mais')); ?>
+          <?php endif; ++$row; ?>
           <h3><?php the_title(); ?></h3>
-          <?php
-              if (!has_excerpt()) {
-              $content = apply_filters('get_the_content', get_the_content());
-              $content = strip_tags($content);
-              $content = mb_strimwidth($content, 0, 500, ' ...');
-              echo '<p>' . $content . '</p>';
-              } else {
-              echo '<p>' . substr(get_the_excerpt(), 0, 450) . '...' . '</p>';
-              }
-          ?>
         </a>
       </article>
     <?php endwhile; ?>
@@ -139,7 +105,6 @@
         <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
       <?php endif; ?>
     </section>
-
   </div>
-
+  <a class="button text-center" href="/categorias">Demais Matérias</a>
 </section>
