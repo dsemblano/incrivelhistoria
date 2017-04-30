2c80950654c16cacec977c765e4bd0a9c30eab05
Daniel Semblano <dsemblano@gmail.com>
2017-04-30 18:42:47 -0300

    pagina busca layout igual ao categorias

--- site/web/app/themes/ii/resources/views/partials/content-search.blade.php
+++ site/web/app/themes/ii/resources/views/partials/content-search.blade.php
@@ -1,11 +1,9 @@
-<article @php(post_class())>
-  <header>
+<article @php(post_class('row no-gutters'))>
+  <figure class="col-sm">
+    <a href="{{ get_permalink() }}"><?php echo (App\featured_image_url('mais_extendida')); ?></a>
+  </figure>
+  <header class="col-sm">
+    @include('partials/entry-meta')
     <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-    @if (get_post_type() === 'post')
-      @include('partials/entry-meta')
-    @endif
   </header>
-  <div class="entry-summary">
-    @php(the_excerpt())
-  </div>
 </article>
