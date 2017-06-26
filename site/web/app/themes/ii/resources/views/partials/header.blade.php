eca9a365ec638b540fcfaa52379ee3e0881a7285
Daniel Semblano <dsemblano@gmail.com>
2017-06-26 01:57:08 -0300

    Ajustes mobile v1.2

--- site/web/app/themes/ii/resources/views/partials/header.blade.php
+++ site/web/app/themes/ii/resources/views/partials/header.blade.php
@@ -33,7 +33,8 @@
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
    aria-label="Toggle navigation">
-     <span class="navbar-toggler-icon"></span>
+     {{-- <span class="navbar-toggler-icon"></span> --}}
+     <i class="fa fa-bars hidden-lg-up" aria-hidden="true"></i>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
