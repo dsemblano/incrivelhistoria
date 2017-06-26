06e4647a7179bd3a464411a55c8b0ed028cb21f8
Daniel Semblano <dsemblano@gmail.com>
2017-06-26 00:59:30 -0300

    Ajustes mobile

--- site/web/app/themes/ii/resources/views/partials/footer.blade.php
+++ site/web/app/themes/ii/resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
     <div class="row no-gutters">
         @if (has_nav_menu('secondary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'sitemap-footer col-8']) !!}
+        {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'sitemap-footer col-xs-12 col-md-8']) !!}
         @endif
         <?php
           // wp_list_categories( array(
@@ -13,7 +13,7 @@
           //
           // ) );
           ?>
-          <div class="col-4 footer-right">
+          <div class="col-xs-12 col-md-4 footer-right">
             {!! get_search_form(false) !!}
             <div class="facebook-footer">
               <p>
