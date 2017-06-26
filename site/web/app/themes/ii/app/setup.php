c89b314e02776c02bc2211ceea54ea0731b1c473
Daniel Semblano <dsemblano@gmail.com>
2017-06-26 04:08:28 -0300

    Ajustes mobile menu footer tertiary

--- site/web/app/themes/ii/app/setup.php
+++ site/web/app/themes/ii/app/setup.php
@@ -42,7 +42,8 @@ add_action('after_setup_theme', function () {
      */
     register_nav_menus([
         'primary_navigation' => __('Primary Navigation', 'sage'),
-        'secondary_navigation' => __('Secondary Navigation', 'sage')
+        'secondary_navigation' => __('Secondary Navigation', 'sage'),
+        'tertiary_navigation' => __('Tertiary Navigation', 'sage')
     ]);
 
     /**
--- site/web/app/themes/ii/resources/views/partials/footer.blade.php
+++ site/web/app/themes/ii/resources/views/partials/footer.blade.php
@@ -2,8 +2,8 @@
   <div class="container">
     {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
     <div class="row no-gutters">
-        @if (has_nav_menu('secondary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'sitemap-footer col-xs-12 col-md-8']) !!}
+        @if (has_nav_menu('tertiary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'tertiary_navigation', 'menu_class' => 'sitemap-footer col-xs-12 col-md-8']) !!}
         @endif
         <?php
           // wp_list_categories( array(
