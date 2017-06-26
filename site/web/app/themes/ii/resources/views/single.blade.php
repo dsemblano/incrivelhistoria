9bb8192de4694d1728b24a9df7b02778109cb2ba
Daniel Semblano <dsemblano@gmail.com>
2017-06-26 07:39:31 -0300

    Ajustes posts e related posts

--- site/web/app/themes/ii/resources/views/single.blade.php
+++ site/web/app/themes/ii/resources/views/single.blade.php
@@ -3,5 +3,6 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     @include('partials.content-single-'.get_post_type())
+    {!! do_shortcode('[crp limit="6" heading="1" cache="1"]') !!}
   @endwhile
 @endsection
