56129fc3307e2dff7d41fb7db5bb2064a83ca1c4
Daniel Semblano <dsemblano@gmail.com>
2017-07-03 18:58:28 -0300

    cor correta tags sidebar e condicional template home nao exibir div header vazia

--- site/web/app/themes/ii/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/ii/resources/views/partials/page-header.blade.php
@@ -1,6 +1,4 @@
 <div class="page-header">
-@if (! is_front_page())
   <h1>{!! App\title() !!}</h1>
   <hr />
-@endif
 </div>
--- site/web/app/themes/ii/resources/views/template-home.blade.php
+++ site/web/app/themes/ii/resources/views/template-home.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
+    {{-- @include('partials.page-header') --}}
     @include('partials.content-page')
     @include('partials.home-slideshow')
     @include('partials.home-curiosidadeselistas')
