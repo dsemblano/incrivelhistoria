239212b3b3712236fda7dce747b7cf6db723339a
Daniel Semblano <dsemblano@gmail.com>
2017-07-17 17:07:22 -0300

    Novo favicon v1.1

--- site/web/app/themes/ii/resources/views/partials/head.blade.php
+++ site/web/app/themes/ii/resources/views/partials/head.blade.php
@@ -3,15 +3,15 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="keywords" content="história, guerra, brasil, nobel, mundial, listas, biografias">
-  <link rel="apple-touch-icon" sizes="180x180" href="@asset("images/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="@asset("images/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="@asset("images/favicon/favicon-16x16.png">
-  <link rel="manifest" href="@asset("images/favicon/manifest.json">
-  <link rel="mask-icon" href="@asset("images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
+  <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
+  <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
+  <link rel="manifest" href="@asset('images/favicon/manifest.json')">
+  <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#5bbad5">
   <meta name="apple-mobile-web-app-title" content="Incr&iacute;vel Hist&oacute;ria">
   <meta name="application-name" content="Incr&iacute;vel Hist&oacute;ria">
   <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="msapplication-TileImage" content="@asset("images/favicon/mstile-144x144.png">
-  <meta name="theme-color" content="#ffffff">  
+  <meta name="msapplication-TileImage" content="@asset('images/favicon/mstile-144x144.png')">
+  <meta name="theme-color" content="#ffffff">
   @php(wp_head())
 </head>
