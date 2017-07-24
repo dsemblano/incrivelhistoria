4be474089afe836a2215b118d1ac960248543839
Daniel Semblano <dsemblano@gmail.com>
2017-07-24 10:00:14 -0300

    Teste font google fonts

--- site/web/app/themes/ii/app/setup.php
+++ site/web/app/themes/ii/app/setup.php
@@ -14,6 +14,7 @@ use Roots\Sage\Template\BladeProvider;
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+    wp_enqueue_style( 'google_fonts', '//fonts.googleapis.com/css?family=Muli:400,700|Open+Sans:400,400i', false, null );
 }, 100);
 
 /**
