dbad0d1f7df6da54c458d9d883f3770dda91c5a9
Daniel Semblano <dsemblano@gmail.com>
2017-08-17 10:44:37 -0300

    otimizações html

--- site/web/app/themes/ii/resources/views/partials/header.blade.php
+++ site/web/app/themes/ii/resources/views/partials/header.blade.php
@@ -26,21 +26,6 @@
         {{-- {{ get_bloginfo('name', 'display') }} --}}
         {{-- <img src="@asset('images/logo.svg')" alt="Incrível História" /> --}}
         <svg
-   xmlns:dc="http://purl.org/dc/elements/1.1/"
-   xmlns:cc="http://creativecommons.org/ns#"
-   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   xmlns:svg="http://www.w3.org/2000/svg"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   version="1.1"
-   id="Layer_1"
-   x="0px"
-   y="0px"
-   viewBox="0 0 315 130.64896"
-   xml:space="preserve"
-   sodipodi:docname="IHa2.svg"
-   inkscape:version="0.92.1 r"
    width="315"
    height="130.64896"><metadata
      id="metadata67"><rdf:RDF><cc:Work
