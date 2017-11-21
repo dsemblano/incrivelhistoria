3b2a2ced2ad865552a7d964849fd56515c949944
Daniel Semblano <dsemblano@gmail.com>
2017-11-21 01:51:22 -0300

    Comentarios facebook

--- site/web/app/themes/ii/resources/views/partials/content-single.blade.php
+++ site/web/app/themes/ii/resources/views/partials/content-single.blade.php
@@ -9,8 +9,9 @@
   <div class="entry-content">
     @php(the_content())
   </div>
-  {{-- <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer> --}}
+  <footer>
+    {{-- {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!} --}}
+    <div class="fb-comments" data-href="https://incrivelhistoria.com.br" data-numposts="10"></div>
+  </footer>
   {{-- @php(comments_template('/partials/comments.blade.php')) --}}
 </article>
