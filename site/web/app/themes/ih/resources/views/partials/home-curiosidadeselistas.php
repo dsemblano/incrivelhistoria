63587ef450967d5a2682eb8a7ce2e6be62f84611
Daniel Semblano <dsemblano@gmail.com>
2018-01-30 17:56:57 -0200

    ativacao plugins composer e volta backup sage v1.1

---
+++ site/web/app/themes/ih/resources/views/partials/home-curiosidadeselistas.php
@@ -0,0 +1,50 @@
+
+<section id="curiosidades">
+  <h2>Curiosidades e Listas</h2>
+  <div class="row">
+    <?php
+        $args = new WP_Query( array( 'category_name' => 'curiosidades', 'posts_per_page'=>4 ) ); $i = 0;
+        if ( $args->have_posts() ): while ( $args->have_posts() ) : $args->the_post(); $i++;
+    ?>
+
+    <?php if ($i == 1): ?>
+    <div class="col-md-8">
+      <article>
+        <a href="<?php the_permalink(); ?>">
+          <?php echo (App\featured_image_url('curiosidades_large')); ?>
+          <h3><?php the_title(); ?></h3>
+          <p>
+            <?php
+              if (!has_excerpt()) {
+              $content = apply_filters('get_the_content', get_the_content());
+              $content = strip_tags($content);
+              $content = mb_strimwidth($content, 0, 500, ' ...');
+              
+              echo '$content';
+              } else {
+              echo substr(get_the_excerpt(), 0, 450) . '...';
+              }
+            ?>
+          </p>
+        </a>
+      </article>
+    </div>
+      <div class="col-md-4">
+      <?php else : ?>
+        <article>
+            <a href="<?php the_permalink(); ?>">
+                <?php echo (App\featured_image_url('curiosidades_small')); ?>
+                  <h4><?php the_title(); ?></h4>
+            </a>
+        </article>
+      <?php endif; ?>
+      <?php endwhile; ?>
+  <?php wp_reset_postdata(); ?>
+      </div>
+    <?php else : ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
+  </div>
+
+
+</section>
---
+++ site/web/app/themes/ih/resources/views/partials/searchform.blade.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="search-form" action="{{ home_url('/') }}">
+  <label>
+    <span class="screen-reader-text">Busca:</span>
+    <input class="search-field" placeholder="Busca" value="" name="s" type="search">
+  </label>
+  <input class="search-submit" value="&#xf002;" type="submit">
+</form>
