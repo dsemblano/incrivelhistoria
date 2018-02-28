b0ed0c990d3a7d2bc43d70452f3a74dbd2c3f58f
Daniel Semblano <dsemblano@gmail.com>
2018-02-27 22:04:59 -0300

    Novos favicons

--- site/web/app/themes/IH/resources/views/partials/head.blade.php
+++ site/web/app/themes/IH/resources/views/partials/head.blade.php
@@ -3,15 +3,23 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="keywords" content="história, guerra, brasil, nobel, mundial, listas, biografias">
-  <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
+  <link rel="apple-touch-icon" sizes="57x57" href="@asset('images/favicon/apple-icon-57x57.png')">
+  <link rel="apple-touch-icon" sizes="60x60" href="@asset('images/favicon/apple-icon-60x60.png')">
+  <link rel="apple-touch-icon" sizes="72x72" href="@asset('images/favicon/apple-icon-72x72.png')">
+  <link rel="apple-touch-icon" sizes="76x76" href="@asset('images/favicon/apple-icon-76x76.png')">
+  <link rel="apple-touch-icon" sizes="114x114" href="@asset('images/favicon/apple-icon-114x114.png')">
+  <link rel="apple-touch-icon" sizes="120x120" href="@asset('images/favicon/apple-icon-120x120.png')">
+  <link rel="apple-touch-icon" sizes="144x144" href="@asset('images/favicon/apple-icon-144x144.png')">
+  <link rel="apple-touch-icon" sizes="152x152" href="@asset('images/favicon/apple-icon-152x152.png')">
+  <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-icon-180x180.png')">
+  <link rel="icon" type="image/png" sizes="192x192"  href="@asset('images/favicon/android-icon-192x192.png')">
   <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
+  <link rel="icon" type="image/png" sizes="96x96" href="@asset('images/favicon/favicon-96x96.png')">
   <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
   <link rel="manifest" href="@asset('images/favicon/manifest.json')">
-  <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#5bbad5">
-  <meta name="apple-mobile-web-app-title" content="Incr&iacute;vel Hist&oacute;ria">
-  <meta name="application-name" content="Incr&iacute;vel Hist&oacute;ria">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="msapplication-TileImage" content="@asset('images/favicon/mstile-144x144.png')">
+  <meta name="msapplication-TileColor" content="#ffffff')">
+  <meta name="msapplication-TileImage" content="@asset('images/favicon/ms-icon-144x144.png')">
+  <meta name="theme-color" content="#ffffff">
   <meta name="theme-color" content="#c27525">
   <meta property="fb:pages" content="184365508715047" />
   <style>
