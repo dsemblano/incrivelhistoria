8a5dbad0fed8f7276837bf97524b59f1144c078a
Daniel Semblano <dsemblano@gmail.com>
2018-03-05 11:14:05 -0300

    Todos os scripts para o footer

--- site/web/app/themes/IH/app/extra.php
+++ site/web/app/themes/IH/app/extra.php
@@ -117,3 +117,15 @@ function wpb_author_info_box( $content ) {
    
   // Allow HTML in author bio section 
   remove_filter('pre_user_description', 'wp_filter_kses');
+
+  //
+  function remove_head_scripts() { 
+    remove_action('wp_head', 'wp_print_scripts'); 
+    remove_action('wp_head', 'wp_print_head_scripts', 9); 
+    remove_action('wp_head', 'wp_enqueue_scripts', 1);
+ 
+    add_action('wp_footer', 'wp_print_scripts', 5);
+    add_action('wp_footer', 'wp_enqueue_scripts', 5);
+    add_action('wp_footer', 'wp_print_head_scripts', 5); 
+ } 
+ add_action( 'wp_enqueue_scripts', 'remove_head_scripts' );
