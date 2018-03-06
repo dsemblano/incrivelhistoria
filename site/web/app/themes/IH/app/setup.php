6a3bf312f141a690f9ec160a70b752a953b5c113
Daniel Semblano <dsemblano@gmail.com>
2018-03-06 16:40:02 -0300

    loadCSS v2

--- site/web/app/themes/IH/app/setup.php
+++ site/web/app/themes/IH/app/setup.php
@@ -13,7 +13,6 @@ use Roots\Sage\Template\BladeProvider;
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
-    wp_enqueue_script('sage/loadCSS.js', asset_path('scripts/loadCSS.js'), false);
     // wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css?family=Muli|Open+Sans', false, null);
 }, 100);
 
