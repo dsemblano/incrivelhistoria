b78fc5b92b569b89cb821e2c5cbf86f9b0841faf
Daniel Semblano <dsemblano@gmail.com>
2018-06-19 16:16:47 -0300

    h1 to h5 same styling, h6 to references and summary

--- site/web/app/themes/IH/app/extra.php
+++ site/web/app/themes/IH/app/extra.php
@@ -28,14 +28,14 @@ function special_nav_class( $classes, $item, $args ) {
     return $classes;
 }
 
-add_action('wp_print_scripts', 'wsds_detect_enqueued_scripts');
+// add_action('wp_print_scripts', 'wsds_detect_enqueued_scripts');
 
-function wsds_detect_enqueued_scripts() {
-    global $wp_scripts;
-    foreach ($wp_scripts->queue as $handle) :
-        echo "AQUI: ". $handle . ' | ';
-    endforeach;
-}
+// function wsds_detect_enqueued_scripts() {
+//     global $wp_scripts;
+//     foreach ($wp_scripts->queue as $handle) :
+//         echo "AQUI: ". $handle . ' | ';
+//     endforeach;
+// }
 
 // defer async
 function jquery_async_defer_attribute($tag, $handle){
