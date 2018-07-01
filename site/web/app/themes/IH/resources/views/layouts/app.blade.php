f81a7ae6d0410e4b842c8528f0650ed638df011c
Daniel Semblano <dsemblano@gmail.com>
2018-07-01 15:46:26 -0300

    facebook comments

--- site/web/app/themes/IH/resources/views/layouts/app.blade.php
+++ site/web/app/themes/IH/resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html @php language_attributes() @endphp>
   @include('partials.head')
    <body @php body_class() @endphp>
-    {{-- @include('partials.facebook') --}}
+    @include('partials.facebook')
     @php do_action('get_header') @endphp
     @include('partials.header')
     <div class="wrap container" role="document">
