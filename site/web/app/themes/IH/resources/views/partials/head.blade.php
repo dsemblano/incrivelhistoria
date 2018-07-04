e580d5db5d39927c52ef27c89a289df35c9b9f2b
Daniel Semblano <dsemblano@gmail.com>
2018-07-04 00:42:11 -0300

    Front end checklist: viewport-fit=cover, new favicons v1.1

--- site/web/app/themes/IH/resources/views/partials/head.blade.php
+++ site/web/app/themes/IH/resources/views/partials/head.blade.php
@@ -3,12 +3,12 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
   <meta name="keywords" content="história, guerra, brasil, nobel, mundial, listas, biografias">
-  <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png?v=M4yerWPwnp')">
-  <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png?v=M4yerWPwnp')">
-  <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png?v=M4yerWPwnp')">
-  <link rel="manifest" href="@asset('images/favicon/site.webmanifest?v=M4yerWPwnp')">
-  <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg?v=M4yerWPwnp')" color="#c27525">
-  <link rel="shortcut icon" href="@asset('images/favicon/favicon.ico?v=M4yerWPwnp')">
+  <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png?')">
+  <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png?')">
+  <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png?')">
+  <link rel="manifest" href="@asset('images/favicon/site.webmanifest?')">
+  <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg?')" color="#c27525">
+  <link rel="shortcut icon" href="@asset('images/favicon/favicon.ico?')">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="apple-mobile-web-app-title" content="Incr&iacute;vel Hist&oacute;ria">
