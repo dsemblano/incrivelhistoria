5a519478f4aa8d7c1b535735032ed996952d0026
Daniel Semblano <dsemblano@gmail.com>
2018-07-04 01:27:17 -0300

    Front end checklist: viewport-fit=cover, new favicons v2

--- site/web/app/themes/IH/resources/views/partials/head.blade.php
+++ site/web/app/themes/IH/resources/views/partials/head.blade.php
@@ -3,9 +3,11 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
   <meta name="keywords" content="história, guerra, brasil, nobel, mundial, listas, biografias">
+
   <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
   <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
   <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
+  <link rel="icon" type="image/png" sizes="192x192" href="@asset('images/favicon/android-chrome-192x192.png')">
   <link rel="manifest" href="@asset('images/favicon/site.webmanifest')">
   <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#c27525">
   <link rel="shortcut icon" href="@asset('images/favicon/favicon.ico')">
@@ -13,7 +15,9 @@
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="apple-mobile-web-app-title" content="Incr&iacute;vel Hist&oacute;ria">
   <meta name="application-name" content="Incr&iacute;vel Hist&oacute;ria">
+  <meta name="msapplication-config" content="@asset('images/favicon/browserconfig.xml')">
   <meta name="msapplication-TileColor" content="#c27525">
+  <meta name="msapplication-TileImage" content="@asset('images/favicon/mstile-144x144.png')">
   <meta name="theme-color" content="#ffffff">
   <meta property="fb:pages" content="184365508715047" />
   @include('partials.analytics')
