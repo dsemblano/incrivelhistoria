4cb5e03714407946de10bb18899ce87602ccb285
Daniel Semblano <dsemblano@gmail.com>
2018-08-21 15:41:16 -0300

    404 customised page v1.2

--- site/web/app/themes/IH/resources/views/404.blade.php
+++ site/web/app/themes/IH/resources/views/404.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   @include('partials.page-header')
 
-  @if (!have_posts())
+  {{-- @if (!have_posts()) --}}
     <div class="alert">
       {{ __('Desculpe, a página acessada não existe. Por favor faça uma nova busca, use um dos links da página ou clique na imagem abaixo para ir na página principal ', 'sage') }}
       <a href="{{ home_url('/') }}">
@@ -11,7 +11,7 @@
       </a>
       {!! get_search_form(false) !!}
     </div>
-  @endif
+  {{-- @endif --}}
 
   {!! get_the_posts_navigation() !!}
 @endsection
