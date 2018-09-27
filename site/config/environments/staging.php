7a2b91332e856d8926765fab52a04bb20d9032dd
Daniel Semblano <dsemblano@gmail.com>
2018-09-27 16:09:05 -0300

    plugins update composer, bedrock update, php-widget uninstalled

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -2,9 +2,7 @@
 /**
  * Configuration overrides for WP_ENV === 'staging'
  */
-
 use Roots\WPConfig\Config;
-
 /**
  * You should try to keep staging as close to production as possible. However,
  * should you need to, you can always override production configuration values
