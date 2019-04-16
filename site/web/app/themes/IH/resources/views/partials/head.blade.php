3b04888e12065c4110fb26651678140ce5c9a64b
Daniel Semblano <dsemblano@gmail.com>
2019-04-16 12:12:54 -0300

    Bootstrap grid ajustments

--- site/web/app/themes/IH/resources/views/partials/head.blade.php
+++ site/web/app/themes/IH/resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="initial-scale=1.0, width=device-width">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="keywords" content="história, guerra, brasil, nobel, mundial, listas, biografias">
 
   <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
