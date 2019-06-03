78d2d64b47e5c472846c7cb7f96f3117b95869a6
Daniel Semblano <dsemblano@gmail.com>
2019-06-03 12:43:40 -0300

    Refactoring: Controller public function popularPost for popularposts.blade v1.1

--- site/web/app/themes/IH/resources/views/partials/popularposts.blade.php
+++ site/web/app/themes/IH/resources/views/partials/popularposts.blade.php
@@ -1,6 +1,12 @@
 <section id="mais-acessadas">
   <h3>Mais acessadas</h3>
   <ul>
+    @if (! $popular_post->have_posts())
+    <div class="alert alert-warning">
+      {{ __('Desculpe, nenhum resultado encontrado.', 'sage') }}
+    </div>
+    @endif
+
     @while ($popular_post->have_posts()) @php $popular_post->the_post() @endphp
     <li>
       <a href={{ the_permalink() }}>{{ the_post_thumbnail('top10') }}</a>
