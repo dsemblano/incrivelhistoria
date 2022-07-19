bc8681f5ef33c1546a8f0ce6b651a5d26bfccd27
Daniel Semblano <dsemblano@gmail.com>
2022-07-19 11:13:28 -0300

    wordpress popular posts shortcode

--- site/web/app/themes/IH/resources/views/partials/popularposts.blade.php
+++ site/web/app/themes/IH/resources/views/partials/popularposts.blade.php
@@ -1,17 +1,4 @@
 <section id="mais-acessadas">
   <h3>Mais acessadas</h3>
-  <ul>
-    @if (! $popular_post->have_posts())
-    <div class="alert alert-warning">
-      {{ __('Desculpe, nenhum resultado encontrado.', 'sage') }}
-    </div>
-    @endif
-
-    @while ($popular_post->have_posts()) @php $popular_post->the_post() @endphp
-    <li>
-      <a href={{ the_permalink() }}>{{ the_post_thumbnail('top10') }}</a>
-    </li>
-    @endwhile
-  </ul>
-  {{-- {!! wpb_set_post_views(get_the_ID()) !!} --}}
+  {!! do_shortcode('[wpp]') !!}
 </section>
