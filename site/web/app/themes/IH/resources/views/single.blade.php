65b717e17c614b88211de039e7d23d3406becc37
Daniel Semblano <dsemblano@gmail.com>
2022-09-14 15:28:11 -0300

    Kadu ads inside single

--- site/web/app/themes/IH/resources/views/single.blade.php
+++ site/web/app/themes/IH/resources/views/single.blade.php
@@ -3,6 +3,7 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-single-'.get_post_type())
+    @include('partials.kaduads')
     {!! do_shortcode('[crp limit="6" heading="1" cache="1"]') !!}
   @endwhile
 @endsection
